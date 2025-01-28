4acbed6abbb20b962a27729ab516809c25b229e4
David Spivack <daspivack@example.com>
2025-01-28 15:57:00 -0800

    get Term

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 struct Result {
-  vector<double> quotient;
-  vector<double> remainder;
+  vector<int> quotient;
+  vector<int> remainder;
 };
 
-vector<double> constructPolynomial(string input) {
+vector<int> constructPolynomial(string input) {
   stringstream ss(input);
-  vector<double> polynomial;
+  vector<int> polynomial;
   string coefficient;
   while (getline(ss, coefficient, ' ')) {
     // toDO: error handling for stod
@@ -22,14 +22,14 @@ vector<double> constructPolynomial(string input) {
   return polynomial;
 }
 
-Result dividePolynomial(vector<double> numerator, vector<double> denominator) {
-  vector<double> quotient;
-  vector<double> product;
-  vector<double> remainder(quotient.size());
+Result dividePolynomial(const vector<int> &numerator, vector<int> denominator) {
+  vector<int> quotient;
+  vector<int> product;
+  vector<int> remainder(quotient.size());
 
-  const double leadingTermN = numerator[0];
+  const int leadingTermN = numerator[0];
   const size_t degreeN = numerator.size() - 1;
-  const double leadingTermD = denominator[0];
+  const int leadingTermD = denominator[0];
   const size_t degreeD = denominator.size() - 1;
 
   quotient.push_back(leadingTermN / leadingTermD);
@@ -45,36 +45,56 @@ Result dividePolynomial(vector<double> numerator, vector<double> denominator) {
     product.push_back(0);
   }
 
-  for (size_t i = 0; i < numerator.size(); i++) {
+  for (size_t i = 1; i < numerator.size(); i++) {
     remainder.push_back(numerator[i] - product[i]);
   }
 
   return {quotient, remainder};
 }
 
+string getTerm(const vector<int> &polynomial) {
+  for (size_t i = 0; i < polynomial.size(); i++) {
+    if (polynomial[i] != 0) {
+      const string coeff = to_string(polynomial[i]);
+      if (polynomial.size() - 1 - i == 0) {
+        return coeff;
+      } else if (polynomial.size() - 1 - i == 1) {
+        return coeff + "x";
+      }
+      const string degree = to_string(polynomial.size() - 1 - i);
+
+      return coeff + "x^" + degree;
+    }
+  }
+  return "";
+}
+
 int main() {
   //   string numeratorAsString = getString("Enter the numerator: ");
   //   string denominatorAsString = getString("Enter the denominator: ");
-  //   vector<double> numerator = constructPolynomial(numeratorAsString);
+  //   vector<int> numerator = constructPolynomial(numeratorAsString);
   //   denominator = constructPolynomial(denominatorAsString);
-  vector<double> numerator = {1, 5, 6};
-  vector<double> denominator = {1, 2};
-  vector<double> quotient;
-  vector<double> newNumerator;
+  vector<int> numerator = {2, 8, -6, 10};
+  vector<int> denominator = {1, -2};
+  vector<int> quotient;
+
+  string displayResult = "";
 
   Result result = dividePolynomial(numerator, denominator);
 
+  displayResult += getTerm(result.quotient);
+
   result = dividePolynomial(result.remainder, denominator);
+
+  displayResult += " + " + getTerm(result.quotient);
+
+  result = dividePolynomial(result.remainder, denominator);
+
+  displayResult += " + " + getTerm(result.quotient);
 
   for (size_t i = 0; i < result.remainder.size(); i++) {
     print(result.remainder[i]);
   }
 
-  //   print(leadingTermN, 0);
-  //   print(" ");
-  //   print(degreeN);
-
-  //   print(leadingTermD, 0);
-  //   print(" ");
-  //   print(degreeD);
+  print(displayResult);
 }
